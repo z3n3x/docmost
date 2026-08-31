c52e8cc63fe679f2d96b551cd48941fe3052418c
z3n3x <60400515+z3n3x@users.noreply.github.com>
2026-08-31 10:43:05 +0300

    feat(i18n): make Russian the default locale

--- apps/client/src/i18n.ts
+++ apps/client/src/i18n.ts
@@ -3,26 +3,20 @@ import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
   .use(Backend)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: "en-US",
+    lng: "ru-RU",
+    fallbackLng: "ru-RU",
     debug: false,
     showSupportNotice: false,
-    load: 'currentOnly',
-
+    load: "currentOnly",
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false,
     },
     react: {
       useSuspense: false,
-    }
+    },
   });
 
 export default i18n;
